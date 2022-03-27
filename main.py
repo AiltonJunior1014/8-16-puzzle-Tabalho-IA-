2957300eb2f6ff75bb8674c4d69a9e84e161bc43
Ailton Junior <ailton.fabricio@hotmail.com>
2022-03-27 18:30:52 -0300

    finalizando movimentos e embaaralhamento

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from matriz import Matriz
 
 mat = Matriz(3, 3)
 
-mat.Monta()
-mat.Mostra()
-mat.Esquerda()
-mat.Mostra()
+mat.monta()
+mat.mostra()
+#mat.sobe()
+for x in range (4):
+    mat.embaralha()
+mat.mostra()
 
 
--- matriz.py
+++ matriz.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 class Matriz():
 
@@ -6,29 +7,25 @@ class Matriz():
         self.lin = linhas
         self.col = colunas
         self.mat = np.zeros((linhas,colunas), dtype=int)
-        self.posx=0
-        self.posy=0
+        self.posx = 0
+        self.posy = 0
 
-
-    def monta(self,num):
+    def monta(self, num):
         cont = 0
         for x in range (self.lin, 2):
             for y in range (self.col/2):
                 self.mat[x][y] = num [cont]
-                cont+=1
+                cont += 1
 
-
-    def Monta (self):
-        aux =0
+    def monta(self):
+        aux = 0
         for x in range (self.lin):
             for y in range (self.col):
                 self.mat[x][y] = aux
                 aux += 1
-        self.posX=x
-        self.posY=y
 
 
-    def GetMatriz(self):
+    def getMatriz(self):
         return self.mat
 
     def isEqual(self,matriz):
@@ -36,37 +33,61 @@ class Matriz():
 
 
 
-    def Sobe(self):
-        Aux = self.mat[self.posx][self.posy]
-        self.mat[self.posx][self.posy] = self.mat[self.posx-1][self.posy]
-        self.mat[self.posx - 1][self.posy ] = Aux
-        self.posx = self.posx-1
+    def sobe(self):
+        Aux = self.mat[self.posy][self.posx]
+        self.mat[self.posy][self.posx] = self.mat[self.posy-1][self.posx]
+        self.mat[self.posy - 1][self.posx ] = Aux
+        self.posy = self.posy-1
 
-    def Desce(self):
-        Aux = self.mat[self.posx][self.posy]
-        self.mat[self.posx][self.posy] = self.mat[self.posx+1][self.posy]
-        self.mat[self.posx + 1][self.posy] = Aux
-        self.posx = self.posx+1
+    def desce(self):
+        Aux = self.mat[self.posy][self.posx]
+        self.mat[self.posy][self.posx] = self.mat[self.posy+1][self.posx]
+        self.mat[self.posy + 1][self.posx] = Aux
+        self.posy = self.posy+1
 
 
-    def Direita(self):
-        Aux = self.mat[self.posx][self.posy]
-        self.mat[self.posx][self.posy] = self.mat[self.posx][self.posy+1]
-        self.mat[self.posx][self.posy + 1] = Aux
+    def direita(self):
+        Aux = self.mat[self.posy][self.posx]
+        self.mat[self.posy][self.posx] = self.mat[self.posy][self.posx+1]
+        self.mat[self.posy][self.posx + 1] = Aux
 
-    def Esquerda(self):
-        Aux = self.mat[self.posx][self.posy]
-        self.mat[self.posx][self.posy] = self.mat[self.posx][self.posy+1]
-        self.mat[self.posx][self.posy + 1] = Aux
+    def esquerda(self):
+        Aux = self.mat[self.posy][self.posx]
+        self.mat[self.posy][self.posx] = self.mat[self.posy][self.posx+1]
+        self.mat[self.posy][self.posx + 1] = Aux
 
-    def Mostra(self):
-        for x in range(self.lin, 2):
-            for y in range(self.col/2):
-                print(self.mat[x][y])
-            print("\n")
+    def mostra(self):
         print(self.mat)
         print("" + str(self.lin) + "" + str(self.col))
         print(""+str(self.posx)+""+str(self.posy))
 
+    def podeSubir(self):
+        return not(self.posy == 0)
+
+    def podeDescer(self):
+        return not(self.posy == self.lin-1)
+
+    def podeEsquerda(self):
+        return not(self.posx == 0)
+
+    def podeDireita(self):
+        return not(self.posx == self.col-1)
+
+    def embaralha(self):
+        random.seed()
+        for x in range(self.lin+self.col):
+            num = random.randint(0, 3)
+            if num == 0:
+                if self.podeSubir():
+                    self.sobe()
+            elif num == 1:
+                if self.podeEsquerda():
+                    self.esquerda()
+            elif num == 2:
+                if self.podeDescer():
+                    self.desce()
+            elif num == 3:
+                if self.podeDireita():
+                    self.direita()
 
 
