90a0c11ad92968492e2195bae712c8f51d4db7bb
Ailton Junior <ailton.fabricio@hotmail.com>
2022-03-28 20:08:59 -0300

    Best First

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from matriz import Matriz
 
 
 mat = Matriz(3, 3)
+mat2 = Matriz(3, 3)
 
 mat.monta()
-mat.mostra()
+mat2.monta()
+mat2.mostra()
 #mat.sobe()
 for x in range (4):
     mat.embaralha()
 mat.mostra()
+
+#mat.qtdpecas(mat2.getMatriz())
+
+mat.bestfirst(mat2)
 
 
--- matriz.py
+++ matriz.py
@@ -1,21 +1,34 @@
 import random
+from copy import deepcopy
+
 import numpy as np
 class Matriz():
 
     def __init__(self,linhas,colunas):
-
+        self.g = 0 #distancia do inicio
         self.lin = linhas
         self.col = colunas
         self.mat = np.zeros((linhas,colunas), dtype=int)
         self.posx = 0
         self.posy = 0
 
+    def custo(self, matriz):
+        return self.g + self.qtdpecas(matriz)
+
+    def getg(self):
+        return self.g
     def monta(self, num):
         cont = 0
         for x in range (self.lin, 2):
             for y in range (self.col/2):
                 self.mat[x][y] = num [cont]
                 cont += 1
+
+    def getlin(self):
+        return self.lin
+
+    def getcol(self):
+        return self.col
 
     def monta(self):
         aux = 0
@@ -50,11 +63,13 @@ class Matriz():
         Aux = self.mat[self.posy][self.posx]
         self.mat[self.posy][self.posx] = self.mat[self.posy][self.posx+1]
         self.mat[self.posy][self.posx + 1] = Aux
+        self.posx = self.posx+1
 
     def esquerda(self):
         Aux = self.mat[self.posy][self.posx]
-        self.mat[self.posy][self.posx] = self.mat[self.posy][self.posx+1]
-        self.mat[self.posy][self.posx + 1] = Aux
+        self.mat[self.posy][self.posx] = self.mat[self.posy][self.posx-1]
+        self.mat[self.posy][self.posx - 1] = Aux
+        self.posx = self.posx -1
 
     def mostra(self):
         print(self.mat)
@@ -90,4 +105,50 @@ class Matriz():
                 if self.podeDireita():
                     self.direita()
 
+    def qtdpecas(self, matriz):# distancia para final
+        if not self.isEqual(matriz).all():
+            count = 0
+            for x in range (self.lin):
+                for y in range (self.col):
+                    if self.mat[x][y] != matriz[x][y]:
+                        count += 1
+            return count
 
+    def bestfirst(self, final):
+        res = self
+        caminho = []
+
+
+        while not(self.isEqual(final.getMatriz()).all()):
+            aux = []
+            mov = []
+
+            for i in range(4):
+                aux.append(deepcopy(res))
+
+            if res.podeSubir():
+                aux[0].sobe()
+                mov.append(0)
+            if res.podeDireita():
+                aux[1].direita()
+                mov.append(1)
+            if res.podeEsquerda():
+                aux[2].esquerda()
+                mov.append(2)
+            if res.podeDescer():
+                aux[3].desce()
+                mov.append(3)
+
+            menorcusto = 99999
+            posmenor = 0
+            for i in range(4):
+                cust = aux[i].custo(final.getMatriz())
+                if menorcusto > cust and i in mov:
+                    menorcusto = cust
+                    posmenor = i
+
+            res = aux[posmenor]
+            caminho.append(posmenor)
+            res.g += 1
+            print(res.getg())
+        print(res)
